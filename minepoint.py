43b5f6346db9b8f080d45b9a8f5b3998246f8d39
demiler <seerpout@gmail.com>
2022-03-08 12:49:12 +0300

    added overcomplicated minepoint that has metadata about point such as bombs around, flags and mask

---
+++ minepoint.py
@@ -0,0 +1,108 @@
+from enum import IntEnum
+
+
+class Value(IntEnum):
+    empty = 0b0000
+    one   = 0b0001
+    two   = 0b0010
+    three = 0b0011
+    four  = 0b0100
+    five  = 0b0101
+    six   = 0b0110
+    seven = 0b0111
+    eight = 0b1000
+    bomb  = 0b1111
+
+class Flag(IntEnum):
+    noflag = 0b00
+    guess  = 0b01
+    sure   = 0b10
+
+class Mask(IntEnum):
+    closed  = 0b00
+    pending = 0b11
+    opened  = 0b01
+
+class MinePoint:
+    VFACTOR = 4
+    MFACTOR = 2
+    FFACTOR = 2
+    MSHIFT  = VFACTOR
+    FSHIFT  = VFACTOR + MFACTOR
+
+    VALUE =  (0x1 << VFACTOR) - 1
+    MASK  = ((0x1 << MFACTOR) - 1) << MSHIFT
+    FLAG  = ((0x1 << FFACTOR) - 1) << FSHIFT
+
+    def __init__(self):
+        self.__data = 0
+
+    def __rawFlag(self):
+        return self.__data & MinePoint.FLAG
+
+    def __rawMask(self):
+        return self.__data & MinePoint.MASK
+
+    def __rawValue(self):
+        return self.__data & MinePoint.VALUE
+
+    def __clearFlag(self):
+        self.__data ^= self.__rawFlag()
+
+    def __clearMask(self):
+        self.__data ^= self.__rawMask()
+
+    def __clearValue(self):
+        self.__data ^= self.__rawValue()
+
+    @property
+    def flag(self):
+        return self.__rawFlag() >> MinePoint.FSHIFT
+
+    @flag.setter
+    def flag(self, flag):
+        self.__clearFlag()
+        self.__data |= (flag << MinePoint.FSHIFT) & MinePoint.FLAG
+
+    @property
+    def mask(self):
+        return self.__rawMask() >> MinePoint.MSHIFT
+
+    @mask.setter
+    def mask(self, mask):
+        self.__clearMask()
+        self.__data |= (mask << MinePoint.MSHIFT) & MinePoint.MASK
+
+    @property
+    def value(self):
+        return self.__rawValue()
+
+    @value.setter
+    def value(self, value):
+        self.__clearValue()
+        self.__data |= value & MinePoint.VALUE
+
+    def __eq__(self, oth):
+        if isinstance(oth, Value):
+            return self.value == oth
+        elif isinstance(oth, Mask):
+            return self.mask == oth
+        elif isinstance(oth, Flag):
+            return self.flag == oth
+        raise TypeError(f'Unexpected type {type(oth)} to compare to')
+
+    def set(self, oth):
+        if isinstance(oth, Value):
+            self.value = oth
+        elif isinstance(oth, Mask):
+            self.mask = oth
+        elif isinstance(oth, Flag):
+            self.flag = oth
+        else:
+            raise TypeError(f'Unexpected type {type(oth)} to set to')
+
+    def __repr__(self):
+        return bin(self.__data)
+
+    def __str__(self):
+        return bin(self.__data)
