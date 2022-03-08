f3e6d7fb4f6315a7b3a2765ddf77ef64eec5c6a5
demiler <seerpout@gmail.com>
2022-03-08 14:16:15 +0300

    Added prototype game ui and movement

--- main.py
+++ main.py
@@ -22,7 +22,68 @@ EIGHT = term.color(124)
 BOMB  = term.color(9)
 colors = [ term.normal, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, BOMB ]
 
+
+def printPixel(x, y, pixel, color=term.noraml):
+    print(term.move_xy(x, y) + color + pixel + term.normal)
+
+def printFieldPixel(field, x, y):
+    cell = field[x, y]
+    if cell == Mask.closed: printPixel(x, y, '?')
+    elif cell == Value.bomb: printPixel(x, y, '*', BOMB)
+    elif cell == Value.empty: printPixel(x, y, ' ')
+    else: printPixel(x, y, str(cell), colors[cell.value])
+
+def clamp(min, max, val):
+    if val < min: return min
+    if val > max: return max
+    return val
+
+def printField(field):
+    print(term.clear)
+    for x in range(field.size):
+        for y in range(field.size):
+            printFieldPixel(field, x, y)
+    print(term.normal)
+
+
 field = Field(20, .1)
+cursor = ( 0, 0 )
+movement = { 'up': 'w', 'down': 's', 'left': 'a', 'right': 'd' }
+keys = { 'open': ' ' }
+
+# print('Field with mask:')
+printField(field)
+printPixel(*cursor, '◉')
+
+with term.cbreak():
+    val = ''
+    while val.lower() != 'q':
+        val = term.inkey(timeout=3)
+        if not val: continue
+
+        if val in movement.values():
+            x, y = cursor
+            printFieldPixel(field, x, y)
+            if   val == movement['up']:    y -= 1
+            elif val == movement['down']:  y += 1
+            elif val == movement['left']:  x -= 1
+            elif val == movement['right']: x += 1
+            x = clamp(0, field.size - 1, x)
+            y = clamp(0, field.size - 1, y)
+            cursor = (x, y)
+            printPixel(x, y, '◉')
+        elif val == ' ':
+            bombed = field.reveal(*cursor)
+            printField(field)
+            printPixel(x, y, '◉')
+            if bombed:
+                print('LMAO')
+                break
+
+exit(1)
+
+
+#some debug output
 print(f'Field size: {field.size}x{field.size}')
 print('Bombs:', field.bombs)
 
@@ -38,12 +99,5 @@ print(f'Revealed at {revCoord}:', not field.reveal(revCoord[0], revCoord[1]))
 for x in range(field.size):
     for y in range(field.size):
         print(field[x, y].mask if field[x, y].mask != Mask.closed else ' ', end='')
-    print()
-
-print('Field with mask:')
-for x in range(field.size):
-    for y in range(field.size):
-        if field[x, y] == Mask.closed: print('?', end='')
-        else: printCell(field[x, y])
     print()
 
