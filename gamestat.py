8c762ea707e5797806a8f9d8f5eb66972aeaa233
Andrey Kazarinov <kazarandrey@yandex.ru>
2022-03-24 11:18:44 +0300

    Improve collecting statistics

--- gamestat.py
+++ gamestat.py
@@ -2,36 +2,40 @@
 
 """Handle game statistics."""
 
+import pickle
+
 
 class Stat:
     """Statistics about current game."""
 
     gametime = 0
     win = 0
+    statfile = ""
 
 
-def printstat():
+def stat_menu():
     """Add new statistics in file and print summary statistics."""
-    s = str(Stat.gametime) + ' ' + str(Stat.win) + '\n'
-    with open("stat.txt", 'a') as f:
-        f.write(s)
-    with open("stat.txt", 'r') as f:
-        st = f.read().splitlines()
-    st = [i.split(' ') for i in st]
+    with open(Stat.statfile, "rb") as f:
+        st = pickle.load(f)
+    st.append([Stat.gametime, Stat.win])
+    with open(Stat.statfile, "wb") as f:
+        pickle.dump(st, f)
     bestwintime = -1
     meanwintime = 0
     gamewin = 0
     for i in st:
-        gamewin += int(i[1])
-        if i[1] == '1':
-            meanwintime += float(i[0])
-            if bestwintime == -1 or bestwintime > float(i[0]):
-                bestwintime = float(i[0])
+        gamewin += i[1]
+        if i[1] == 1:
+            meanwintime += i[0]
+            if bestwintime == -1 or bestwintime > i[0]:
+                bestwintime = i[0]
     winpercent = round(gamewin / len(st) * 100)
-    meanwintime /= gamewin
-    print(f"""\nTime: {Stat.gametime} sec
-Best win time: {bestwintime} sec
+    meanwintime = meanwintime / gamewin if gamewin else 0
+    print(
+        f"""\nTime: {Stat.gametime} sec
+Best win time: {bestwintime if bestwintime > 0 else '-'} sec
 Games played: {len(st)}
 Games wined: {gamewin}
 Wins percent: {winpercent}%
-Mean win time: {meanwintime} sec""")
+Mean win time: {meanwintime if meanwintime > 0 else '-'} sec """
+    )
--- main.py
+++ main.py
@@ -5,19 +5,21 @@
 from blessed import Terminal
 from field import Field
 from minepoint import Value, Mask, Flag
-from gamestat import Stat, printstat
+from gamestat import Stat, stat_menu
 import time
+import os
+import pickle
 
 
 def printCell(cell):
     """Draw bomb or empty cell when it open."""
     if cell == Value.bomb:
-        color, char = BOMB, '*'
+        color, char = BOMB, "*"
     elif cell == Value.empty:
-        color, char = term.normal, ' '
+        color, char = term.normal, " "
     else:
         color, char = colors[cell.value], str(cell)
-    print(color + char + term.normal, end='')
+    print(color + char + term.normal, end="")
 
 
 term = Terminal()
@@ -31,6 +33,7 @@ SEVEN = term.color(161)
 EIGHT = term.color(124)
 BOMB = term.color(9)
 colors = [term.normal, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, BOMB]
+username = "Gamer1"
 
 
 def printPixel(x, y, pixel, color=term.noraml):
@@ -43,16 +46,16 @@ def printFieldPixel(field, x, y):
     cell = field[x, y]
     if cell == Mask.closed:
         if cell == Flag.noflag:
-            printPixel(x, y, '?')
+            printPixel(x, y, "?")
         elif cell == Flag.guess:
-            printPixel(x, y, '▻')
+            printPixel(x, y, "▻")
         elif cell == Flag.sure:
-            printPixel(x, y, '►')
+            printPixel(x, y, "►")
     else:
         if cell == Value.bomb:
-            printPixel(x, y, '*', BOMB)
+            printPixel(x, y, "*", BOMB)
         elif cell == Value.empty:
-            printPixel(x, y, ' ')
+            printPixel(x, y, " ")
         else:
             printPixel(x, y, str(cell), colors[cell.value])
 
@@ -75,22 +78,33 @@ def printField(field):
     print(term.normal)
 
 
-field = Field(5, .1)
+field = Field(5, 0.1)
 cursor = (0, 0)
-movement = {'up': 'w', 'down': 's', 'left': 'a', 'right': 'd'}
-keys = {'open': ' '}
+movement = {"up": "w", "down": "s", "left": "a", "right": "d"}
+keys = {"open": " "}
+
+print(f"Input another name if you are not {username}.")
+tmpname = input()
+if tmpname != "":
+    username = tmpname
+Stat.statfile = os.getcwd() + os.sep + "stat" + os.sep + username + ".bin"
+if not os.path.exists(Stat.statfile):
+    open(Stat.statfile, "xb")
+    with open(Stat.statfile, "wb") as f:
+        pickle.dump([], f)
+
 
 # print('Field with mask:')
 printField(field)
-printPixel(*cursor, '◉')
+printPixel(*cursor, "◉")
 activeBombs = field.bombs
 closedCells = field.size * field.size
-print(term.move_xy(0, field.size) + 'CHEAT Bombs remain: ' + str(activeBombs))
+print(term.move_xy(0, field.size) + "CHEAT Bombs remain: " + str(activeBombs))
 
 with term.cbreak(), term.hidden_cursor():
-    val = ''
+    val = ""
     starttime = time.time()
-    while val.lower() != 'q':
+    while val.lower() != "q":
         val = term.inkey(timeout=3)
         if not val:
             continue
@@ -98,34 +112,34 @@ with term.cbreak(), term.hidden_cursor():
         if val in movement.values():
             x, y = cursor
             printFieldPixel(field, x, y)
-            if val == movement['up']:
+            if val == movement["up"]:
                 y -= 1
-            elif val == movement['down']:
+            elif val == movement["down"]:
                 y += 1
-            elif val == movement['left']:
+            elif val == movement["left"]:
                 x -= 1
-            elif val == movement['right']:
+            elif val == movement["right"]:
                 x += 1
             x = clamp(0, field.size - 1, x)
             y = clamp(0, field.size - 1, y)
             cursor = (x, y)
-            printPixel(x, y, '◉')
-        elif val == ' ':
+            printPixel(x, y, "◉")
+        elif val == " ":
             if field[cursor[0], cursor[1]] == Flag.sure:
                 continue
             bombed = field.reveal(*cursor)
             printField(field)
             if bombed:
-                print('LMAO')
+                print("LMAO")
                 Stat.win = 0
                 break
-            printPixel(*cursor, '◉')
+            printPixel(*cursor, "◉")
             closedCells = 0
             for x in range(field.size):
                 for y in range(field.size):
                     if field[x, y] == Mask.closed:
                         closedCells += 1
-        elif val == 'm':
+        elif val == "m":
             cell = field[cursor[0], cursor[1]]
             flag = cell.flag
 
@@ -142,35 +156,43 @@ with term.cbreak(), term.hidden_cursor():
             field[cursor[0], cursor[1]].set(flag)
 
         closedMarkedCells = closedCells - field.bombs + activeBombs
-        print(term.move_xy(0, field.size) + 'CHEAT Bombs remain: ' +
-                                            str(activeBombs) + ' '*10)
-        print(term.move_xy(0, field.size + 1) +
-              'Cells remain: ' + str(closedMarkedCells) + ' '*10)
+        print(
+            term.move_xy(0, field.size)
+            + "CHEAT Bombs remain: "
+            + str(activeBombs)
+            + " " * 10
+        )
+        print(
+            term.move_xy(0, field.size + 1)
+            + "Cells remain: "
+            + str(closedMarkedCells)
+            + " " * 10
+        )
         if closedMarkedCells == 0:
-            print('Good job')
+            print(f"Good job, {username}")
             Stat.win = 1
             break
 
 Stat.gametime = round(time.time() - starttime, 1)
-printstat()
+stat_menu()
 
 exit(1)
 
 
-#some debug output
-print(f'Field size: {field.size}x{field.size}')
-print('Bombs:', field.bombs)
+# some debug output
+print(f"Field size: {field.size}x{field.size}")
+print("Bombs:", field.bombs)
 
-print('Field:')
+print("Field:")
 for x in range(field.size):
     for y in range(field.size):
         printCell(field[x, y])
     print()
 
 revCoord = (field.size // 2, field.size // 2)
-print(f'Revealed at (0, 0):', not field.reveal(0, 0))
-print(f'Revealed at {revCoord}:', not field.reveal(revCoord[0], revCoord[1]))
+print(f"Revealed at (0, 0):", not field.reveal(0, 0))
+print(f"Revealed at {revCoord}:", not field.reveal(revCoord[0], revCoord[1]))
 for x in range(field.size):
     for y in range(field.size):
-        print(field[x, y].mask if field[x, y].mask != Mask.closed else ' ', end='')
+        print(field[x, y].mask if field[x, y].mask != Mask.closed else " ", end="")
     print()
