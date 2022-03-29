ffb8b87369e01b758b75c69811a57b47ab5999f7
demiler <seerpout@gmail.com>
2022-03-29 22:24:42 +0300

    Refactored logic/gamestat

--- logic/gamestat.py
+++ logic/gamestat.py
@@ -3,39 +3,66 @@
 """Handle game statistics."""
 
 import pickle
+import os
 
 
 class Stat:
     """Statistics about current game."""
 
-    gametime = 0
-    win = 0
-    statfile = ""
+    def __init__(self):
+        self.__data = {
+            'gametime': 0,
+            'win': 0,
+        }
+        self.__old = []
+        self.__file = None
 
+    def __getitem__(self, attr):
+        if attr in self.__data:
+            return self.__data[attr]
+        raise ValueError(f'Unknown item {attr} to access statistic')
 
-def stat_menu():
-    """Add new statistics in file and print summary statistics."""
-    with open(Stat.statfile, "rb") as f:
-        st = pickle.load(f)
-    st.append([Stat.gametime, Stat.win])
-    with open(Stat.statfile, "wb") as f:
-        pickle.dump(st, f)
-    bestwintime = -1
-    meanwintime = 0
-    gamewin = 0
-    for i in st:
-        gamewin += i[1]
-        if i[1] == 1:
-            meanwintime += i[0]
-            if bestwintime == -1 or bestwintime > i[0]:
-                bestwintime = i[0]
-    winpercent = round(gamewin / len(st) * 100)
-    meanwintime = meanwintime / gamewin if gamewin else 0
-    print(
-        f"""\nTime: {Stat.gametime} sec
-Best win time: {bestwintime if bestwintime > 0 else '-'} sec
-Games played: {len(st)}
-Games wined: {gamewin}
-Wins percent: {winpercent}%
-Mean win time: {meanwintime if meanwintime > 0 else '-'} sec """
-    )
+    def __setitem__(self, attr, val):
+        if attr in self.__data:
+            self.__data[attr] = val
+        else:
+            raise ValueError(f'Unknown item {attr} to access statistic')
+
+    def assignFile(self, name):
+        self.filepath = os.getcwd() + os.sep + "stat" + os.sep + name + ".bin"
+        if not os.path.exists(self.filepath):
+            with open(self.filepath, 'wb') as f:
+                pickle.dump([], f)
+        return self.filepath
+
+    def readStatistic(self):
+        with open(self.filepath, 'rb') as f:
+            self.__old = pickle.load(f)
+
+    def saveStatistic(self):
+        with open(self.filepath, 'wb') as f:
+            pickle.dump([*self.__old, list(self.__data.values())], f)
+
+    def findBest(self):
+        bestWinTime = self['gametime'] if self['win'] else None
+        meanWinTime = self['gametime'] if self['win'] else None
+        gamesWon = int(self['win'])
+        for history in self.__old:
+            hTime, hWin = history
+            gamesWon += hWin
+            if hWin:
+                meanWinTime += hTime
+                if bestWinTime > hTime:
+                    bestWinTime = hTime
+        meanWinTime = None if gamesWon == 0 else meanWinTime / gamesWon
+        return bestWinTime, meanWinTime, gamesWon
+
+    def print(self):
+        bestWinTime, meanWinTime, gamesWon = self.findBest()
+        winPercent = gamesWon / (len(self.__old) + 1) * 100
+        print('Current game time:', self['gametime'], 'seconds')
+        print('Best win time:    ', bestWinTime, 'seconds')
+        print('Games played:     ', len(self.__old) + 1)
+        print('Games won:        ', gamesWon)
+        print(f'Win percent:       {winPercent:.2f}')
+        print('Mean win time:    ', meanWinTime, 'seconds')
