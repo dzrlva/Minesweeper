836b72adff8b539997b56f18f2e73859037eeefe
demiler <seerpout@gmail.com>
2022-03-29 17:49:26 +0300

    pydocstyle color.py

--- color.py
+++ color.py
@@ -1,3 +1,5 @@
+"""Use colors in terminal with ease."""
+
 DefaultColors = {
     'white':   7,    # noqa: E241
     'black':   232,  # noqa: E241
@@ -19,7 +21,17 @@ DefaultColors = {
 
 
 class Color:
+    """Color class to handle terminal color with escape sequence."""
+
     def __init__(self, fg=None, bg=None):
+        """
+        Create new terminal color.
+
+        fg/bg can be string - one of default colors
+        fg/bg can be int - represents 0-256 ascii colorid
+        fg/bg can be None - in this case fg/bg is not set
+        bg also can be bool - then provided color becomes background only.
+        """
         if isinstance(fg, str):
             if fg not in DefaultColors:
                 raise ValueError(f'Unknown default color: "{fg}"')
@@ -41,6 +53,7 @@ class Color:
             raise TypeError(f'Unknown type for Color background: {type(bg)}')
 
     def __wrap(self):
+        """Wrap colorid with escape sequence."""
         fgw = f'38;5;{self.__fg}' if self.__fg is not None else None
         bgw = f'48;5;{self.__bg}' if self.__bg is not None else None
         if fgw is not None and bgw is not None:
@@ -52,14 +65,17 @@ class Color:
         return ''
 
     def __str__(self):
+        """To use color as terminal color."""
         res = self.__wrap()
         return '\x1b' + res if res else res
 
     def __repr__(self):
+        """Print color to console without actually applying it."""
         res = self.__wrap()
         return '\\x1b' + res if res else '<No color>'
 
     def __add__(self, oth):
+        """Fill unused fg/bg in each color if possible, second color overwrites first."""
         if isinstance(oth, str):
             return str(self) + oth
         if not isinstance(oth, Color):
@@ -69,6 +85,7 @@ class Color:
         return Color(fg, bg)
 
     def __radd__(self, oth):
+        """Fill unused fg/bg in each color if possible, first color overwrites second."""
         if isinstance(oth, str):
             return oth + str(self)
         if not isinstance(oth, Color):
@@ -78,10 +95,13 @@ class Color:
         return Color(fg, bg)
 
     def __invert__(self):
+        """Flip bg and fg of Color."""
         return Color(self.__bg, self.__fg)
 
 
 class ColorMetaClass:
+    """Class for .bg attr in Color."""
+
     pass
 
 
