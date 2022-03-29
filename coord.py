991accd2dbc21d43a42f620375e96ff3773845a3
demiler <seerpout@gmail.com>
2022-03-29 11:39:36 +0300

    Added docstring to Coord

--- coord.py
+++ coord.py
@@ -1,13 +1,21 @@
+"""Simple Coord class module."""
 from random import randint
 
 
 class Coord:
     @staticmethod
     def __convert(val):
+        """Converts value to Coord class if needed."""
         return val if isinstance(val, Coord) else Coord(val)
 
     @staticmethod
     def random(start, end=None):
+        """
+        Generats random coordinate in provided range
+        Provided values converted to Coord if needed
+        By default start = Coord(0, 0), end = value as Coord
+        Result is x = randint(start.x, end.x), y = randint(start.y, end.y)
+        """
         if end is None:
             start, end = 0, start
         start, end = Coord.__convert(start), Coord.__convert(end)
@@ -21,6 +29,12 @@ class Coord:
 
     @staticmethod
     def range(start, end=None, step=None):
+        """
+        Generates range for coordinates
+        Provided values convert to Coord if needed
+        By default start = Coord(0, 0), end = value as Coord, step = Coord(1, 1)
+        Result - all coordinates of a square with certain step
+        """
         if end is None:
             start, end = 0, start
         if step is None:
@@ -35,6 +49,18 @@ class Coord:
                 yield Coord(x, y)
 
     def __init__(self, val, y=None):
+        """
+        Creates Coord instance
+        Takes one or two parameters
+        If only one value is present assuming following:
+        * Coord                  -> just a copy
+        * list/tuple length of 2 -> Coord(val[0], val[1])
+        * dict with 'x' and 'y'  -> Coord(val['x'], val['y'])
+        * int/float              -> Coord(val, val)
+        All values converted to int
+        If both arguments are given, assuming they are numbers:
+        Coord(val, y), both converted to int
+        """
         if y is None:
             if isinstance(val, Coord):
                 self.x, self.y = val.x, val.y
@@ -52,48 +78,60 @@ class Coord:
             self.x, self.y = int(val), int(y)
 
     def __getitem__(self, idx):
+        """Allows to access x/y by 0/1 or a string."""
         if idx == 0 or idx == 'x':
             return self.x
         if idx == 1 or idx == 'y':
             return self.y
 
     def __setitem__(self, idx, val):
+        """Allows to set x/y by 0/1 or a string."""
         if idx == 0 or idx == 'x':
             self.x = int(val)
         if idx == 1 or idx == 'y':
             self.y = int(val)
 
     def __radd__(self, oth):
+        """Addition of two Coord, given value converted if needed."""
         val = Coord.__convert(oth)
         return Coord(val.x + self.x, val.y + self.y)
 
     def __rsub__(self, oth):
+        """Addition of two Coord, given value converted if needed."""
         val = Coord.__convert(oth)
         return Coord(val.x - self.x, val.y - self.y)
 
     def __sub__(self, oth):
+        """Addition of two Coord, given value converted if needed."""
         val = Coord.__convert(oth)
         return Coord(self.x - val.x, self.y - val.y)
 
     def __add__(self, oth):
+        """Addition of two Coord, given value converted if needed."""
         val = Coord.__convert(oth)
         return Coord(self.x + val.x, self.y + val.y)
 
     def __eq__(self, oth):
+        """Addition of two Coord, given value converted if needed."""
         val = Coord.__convert(oth)
         return self.x == val.x and self.y == val.y
 
     def __iter__(self):
+        """Returns iterator for x and y values."""
         return iter((self.x, self.y))
 
     def __neg__(self):
+        """Returns negative of coords."""
         return Coord(-self.x, -self.y)
 
     def __invert__(self):
+        """Flips x and y places."""
         return Coord(self.y, self.x)
 
     def __repr__(self):
+        """Represents Coord in console."""
         return f'Coord <{self.x}, {self.y}>'
 
     def __str__(self):
+        """String converter."""
         return f'({self.x}, {self.y})'
