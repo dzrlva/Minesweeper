f535a73ce752942fd785db6fc1ed5457286f7835
demiler <seerpout@gmail.com>
2022-03-30 13:11:54 +0300

    Removed screen from Game's self, added proper redraw

--- cli/game.py
+++ cli/game.py
@@ -1,44 +1,79 @@
-from .screen import Color
+from .screen import Screen, Color
+from .box import Box
 from util.minepoint import Flag, Mask, Value
 from util.coord import Coord
 
 
 VALUECOLORS = [ 0, 38, 73, 40, 136, 172, 202, 161, 124, 9 ]
 KEYS = { 'up': 'w', 'down': 's', 'left': 'a', 'right': 'd', 'open': ' ', 'mark': 'f' }
+screen = Screen()
 
 
 class Game:
-    def __init__(self, screen, field):
-        self.screen = screen
+    def __init__(self, field):
         self.field = field
-        self.pos = Coord(0, 0)
-        # self.pos = Coord.random(field.size)
+        self.oldPos = None
+        self.pos = Coord.random([field.width, field.height])
         self.cursor = Color(239, bg=True)
         self.stat = field.statistic()
         self.status = 'active'
+        self.box = Box(field.width + 1, field.height + 1, 'soft')
+        self.redraw = True
+        self.fullredraw = True
+        self.barsHeight = 2
 
     def draw(self):
         if self.status == 'active':
-            self.drawField([0, 4])
-            self.drawInfoBars()
-            self.drawCursor([0, 4])
+            if self.fullredraw:
+                self.fullredraw = False
+                self.redraw = True
+                self.box.draw([0, self.barsHeight])
+            if self.redraw:
+                self.redraw = False
+                self.drawField([1, self.barsHeight + 1])
+                self.drawInfoBars()
+            self.drawCursor([1, self.barsHeight + 1])
         elif self.status == 'lose':
-            self.gameover([0, 4])
+            self.gameover([1, self.barsHeight + 1])
         elif self.status == 'win':
-            self.drawField([0, 4])
+            self.drawField([0, self.barsHeight + 1])
             self.win([0, 0])
         else:
-            self.screen[0, 0, Color.red].print(f'[Error] UNKNOWN GAME STATUS: {self.status}').whip()
+            screen[0, 0, Color.red].print(f'[Error] UNKNOWN GAME STATUS: {self.status}').whip()
 
-    def cellInfo(self, cell):
+    def drawCursor(self, offset):
+        offset = Coord(offset)
+        char, color = self.cellInfo(self.pos)
+        screen.drawPixel(self.pos + offset, char, color + self.cursor)
+        if self.oldPos is not None:
+            char, color = self.cellInfo(self.oldPos)
+            screen.drawPixel(self.oldPos + offset, char, color)
+
+    def drawField(self, offset):
+        offset = Coord(offset)
+        for x, y in self.field:
+            char, color = self.cellInfo(x, y)
+            screen.drawPixel(x + offset.x, y + offset.y, char, color)
+
+    def drawInfoBars(self):
+        stat = self.stat
+        screen[1, 0].print(f"Cells: {stat['cellsTotal']}/{self.field.size}").whip()
+        screen[1, 1].print(f"Bombs left: {stat['bombsMarked']}/{self.field.bombs}").whip()
+
+    def cellInfo(self, x, y=None):
         """Draw cell by coordinates."""
+        if y is None:
+            cell = self.field[Coord(x)]
+        else:
+            cell = self.field[Coord(x, y)]
+
         if cell == Mask.closed:
             if cell == Flag.noflag:
                 return ' ', Color.bg.grey
             elif cell == Flag.guess:
-                return 'G', Color.yellow
+                return 'G', Color('yellow', 'grey')
             elif cell == Flag.sure:
-                return 'F', Color.banana
+                return 'F', Color('banana', 'grey')
         else:
             if cell == Value.bomb:
                 return '*', Color.red
@@ -47,25 +82,9 @@ class Game:
             else:
                 return str(cell), Color(VALUECOLORS[cell.value])
 
-    def drawCursor(self, offset):
-        pos = self.pos + Coord(offset)
-        char, color = self.cellInfo(self.field[self.pos])
-        self.screen.drawPixel(pos.x, pos.y, char, color + self.cursor)
-
-    def drawField(self, offset):
-        offset = Coord(offset)
-        for x, y in self.field:
-            char, color = self.cellInfo(self.field[x, y])
-            self.screen.drawPixel(x + offset.x, y + offset.y, char, color)
-
-    def drawInfoBars(self):
-        stat = self.stat
-        self.screen[0, 0].print(f"Cells: {stat['cellsTotal']}/{self.field.size**2}").whip()
-        self.screen[0, 1].print(f"Bombs left: {stat['bombsMarked']}/{self.field.bombs}").whip()
-        self.screen[0, 2].print('[debug]', stat).whip()
-
     def move(self, direct):
         if self.field.inBounds(self.pos + direct):
+            self.oldPos = Coord(self.pos.x, self.pos.y)
             self.pos += direct
 
     def keyAction(self, key):
@@ -84,6 +103,7 @@ class Game:
             elif key == KEYS['open']:
                 if self.field[self.pos] != Flag.sure and self.field.reveal(self.pos):
                     self.status = 'lose'
+            self.redraw = True
             self.checkWin()
 
     def updateStat(self):
@@ -97,15 +117,15 @@ class Game:
 
     def win(self, offset):
         offset = Coord(offset)
-        self.screen[offset, Color.lime].print('Congrats!').whip()
+        screen[offset, Color.lime].print('Congrats!').whip()
 
     def gameover(self, offset):
         offset = Coord(offset)
-        self.screen[0, 0, Color.red].print('You have lost!').whip()
+        screen[0, 0, Color.red].print('You have lost!').whip()
 
         for x, y in self.field:
             if self.field[x, y] == Value.bomb:
                 self.field[x, y] = Mask.opened
-                char, color = self.cellInfo(self.field[x, y])
-                self.screen.drawPixel(x + offset.x, y + offset.y, char, color)
-        self.screen[offset + self.field.size]
+                char, color = self.cellInfo(x, y)
+                screen.drawPixel(x + offset.x, y + offset.y, char, color)
+        screen[offset + self.field.size]
--- main.py
+++ main.py
@@ -9,15 +9,16 @@ from blessed import Terminal
 import time
 
 
-field = Field(5, 0.1)
+field = Field(20, 10, 0.2)
 term = Terminal()
 screen = Screen()
-game = Game(screen, field)
+game = Game(field)
+
 
 username = 'Gamer1'
-tempname = input(f'Input another name if you are not {username}.')
-if tempname:
-    username = tempname
+# tempname = input(f'Input another name if you are not {username}: ')
+# if tempname:
+    # username = tempname
 
 stat = Stat()
 stat.assignFile(username)
@@ -41,5 +42,5 @@ with term.cbreak(), term.hidden_cursor():
 
 stat['gametime'] = round(time.time() - starttime, 1)
 stat.saveStatistic()
-screen.setCursor(0, field.size + 4).setColor(Color.reset)
+screen.setCursor(0, field.height + 6).setColor(Color.reset)
 stat.print()
