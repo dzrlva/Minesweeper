e52f6415939d7be0fabc51082f4aca4fefe68068
demiler <seerpout@gmail.com>
2022-03-31 13:15:35 +0300

    Added basic utils

---
+++ util/__init__.py
@@ -0,0 +1,15 @@
+from PIL import ImageTk, Image
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+def loadImage(imgPath, size=None):
+    if size is None:
+        return ImageTk.PhotoImage(Image.open(imgPath))
+    if isinstance(size, int):
+        size = (size, size)
+    return ImageTk.PhotoImage(Image.open(imgPath).resize(size))
