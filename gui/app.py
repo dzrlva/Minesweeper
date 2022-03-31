418fc1bd0bb18834cd4a83e613e92dfb106b41e7
demiler <seerpout@gmail.com>
2022-03-31 13:28:06 +0300

    new colors

--- gui/app.py
+++ gui/app.py
@@ -4,11 +4,16 @@ import tkinter as tk
 from .board import Board
 
 
+COLORS = {
+    'main': '#003153',
+}
+
+
 class App(tk.Tk):
     def __init__(self, width=900, height=900):
         super().__init__()
         self.title = 'Minesweeper'
-        self.canvas = tk.Canvas(self, width=width, height=width, bg="#ffffff")
+        self.canvas = tk.Canvas(self, width=width, height=width, bg=COLORS['main'])
         self.canvas.pack(expand='no', fill='both')
 
         self.board = Board(self, 12)
