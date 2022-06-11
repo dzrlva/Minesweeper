a89c919e2569326499fd31d06f3645c96f5bdc3f
ozerova daria <ozerova.daria2016@mail.ru>
2022-06-11 12:48:03 +0300

    added SAVE button to Settings Menu

--- gui/menu.py
+++ gui/menu.py
@@ -113,7 +113,6 @@ class NewGameFrame(tk.Frame):
                   command=lambda: master.switch_frame(MainMenu)).pack(side="top", fill="y", padx=30, pady=15)
         tk.Button(self, text="Cancel", font=("Purisa", 13), width=25, height=2,
                   command=lambda: master.switch_frame(MainMenu)).pack(side="top", fill="y", padx=30, pady=5)
-        #center.grid(row=2, column=3)
 
 
 
@@ -125,24 +124,11 @@ class StatisticsFrame(tk.Frame):
         buttons = tk.Frame(self)
         loadgamelb = tk.Listbox(listbox, width=60, height=6)
         loadgamelb.insert(1, "<  player1: Empty  >")
-        loadgamelb.insert(2, "<  player2: Empty  >")
-        loadgamelb.insert(3, "<  player3: Empty  >")
-        loadgamelb.insert(4, "<  player4: Empty  >")
-        loadgamelb.insert(5, "<  player5: Empty  >")
-        loadgamelb.insert(6, "<  player6: Empty  >")
-        loadgamelb.insert(7, "<  player7: Empty  >")
-        loadgamelb.insert(8, "<  player8: Empty  >")
-        loadgamelb.insert(9, "<  player9: Empty  >")
-        loadgamelb.insert(10, "<  player10: Empty  >")
-        loadgamelb.insert(11, "<  player11: Empty  >")
-        loadgamelb.insert(12, "<  player12: Empty  >")
         scrollbar = tk.Scrollbar(listbox, orient="vertical", command=loadgamelb.yview)
         scrollbar.pack(side="right", fill= "y")
         loadgamelb.config(yscrollcommand=scrollbar.set)
         loadgamelb.pack()
-        #tk.Button(buttons, text="CONINUE GAME", state="disabled",
-        #          command=lambda: master.switch_fame(MainMenu)).pack()
-        tk.Button(buttons, text="CANCEL",font=("Purisa", 13), width=35, height=3,
+        tk.Button(buttons, text="Quit",font=("Purisa", 13), width=35, height=3,
                   command=lambda: master.switch_frame(MainMenu)).pack(side="top", fill="y", pady=100, padx=100)
 
         listbox.pack(side="top")
@@ -153,19 +139,16 @@ class SettingsFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         center = tk.Frame(self, borderwidth=0, relief="ridge")
-        left = tk.Frame(self, borderwidth=0, relief="ridge")
-        right = tk.Frame(self, borderwidth=0, relief="ridge")
         bottom = tk.Frame(self, borderwidth=0, relief="solid")
         header = tk.Frame(self, borderwidth=0, relief="ridge")
         tk.Label(bottom, text="\n\n")
         tk.Label(header, text="Settings", font=("Purisa", 23)).pack(side="top", fill="both", pady=50)
-        #tk.Label(left, text="Game settings", font=("Purisa", 15)).pack(side="top", fill="x", pady=0)
         tk.Label(center, text="Language", font=("Purisa", 16)).pack(side="top", fill="x", pady=10, padx=100)
-        resoptions_left = ["English", "Russian"]
-        resvar_left = tk.StringVar(master)
-        resvar_left.set(resoptions_left[0])
-        resmenu_left = tk.OptionMenu(center, resvar_left, *resoptions_left, command=lambda: master.getData())
-        resmenu_left.pack(side="top", fill="x", pady=20)
+        resoptions= ["English", "Russian"]
+        resvar = tk.StringVar(master)
+        resvar.set(resoptions_left[0])
+        resmenu = tk.OptionMenu(center, resvar, *resoptions, command=lambda: master.getData())
+        resmenu.pack(side="top", fill="x", pady=20)
 
         tk.Label(center, text="Colorscheme", font=("Purisa", 16)).pack(side="top", fill="x", pady=10)
         MODES = [("dark", "1"),
@@ -174,21 +157,16 @@ class SettingsFrame(tk.Frame):
         vs.set("1")
         for text, mode in MODES:
             tk.Radiobutton(center, text=text, font=("Purisa", 13), variable=vs, value=mode).pack(side="left", fill="x", padx=50, pady=20)
-        #b1 = tk.Button(center, text="SAVE", font=("Purisa", 10), width=10, height=1,
-        #            command=lambda: master.saved_info(master.switch_frame(MainMenu)))
-        #b3 = tk.Button(bottom, text="HELP", font=("Purisa", 10), width=25, height=2,
-        #               command=lambda: master.show_info())
+        b1 = tk.Button(center, text="SAVE", font=("Purisa", 10), width=10, height=1,
+                    command=lambda: master.saved_info(master.switch_frame(MainMenu)))
         b2 = tk.Button(bottom, text="CANCEL", font=("Purisa", 10), width=25, height=2,
                     command=lambda: master.switch_frame(MainMenu))
 
 
-        #b1.pack(side="bottom", padx=20)
+        b1.pack(side="bottom", padx=20, pady=20)
         b2.pack(side="bottom", padx=20, pady=20)
-        #b3.pack(side="bottom", padx=20, pady=10)
 
         header.grid(row=1, column=3)
-        left.grid(row=2, column=2)
-        right.grid(row=2, column=4)
         bottom.grid(row=5, column=3)
         center.grid(row=2, column=3)
 
